1b6478367dcbdf7ae022c224e89af3ee2a58e23e
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-03-10 21:31:23 +0430

    resposev and fix randomcolor

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ generateBtn.addEventListener("click", generateColor);
 
 function generateColor() {
   for (let i = 0; i < colorPlace.length; i++) {
-    const randomColor = Math.floor(Math.random() * 122000).toString(16);
+    const randomColor = Math.floor(Math.random() * 124777).toString(16);
     colorPlace[i].style.background = "#" + randomColor;
     colorName[i].textContent = "#" + randomColor;
     colorPlace[i].classList.remove("animate");
