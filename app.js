d57c67971a8ef807ebc5e286103c6eee25c3aa0e
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-03-10 21:20:53 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let generateBtn = document.querySelector(".generateBtn");
+let colorPlace = document.querySelectorAll(".colors");
+let colorName = document.querySelectorAll(".colorName");
+let colorNameList = document.querySelector(".colorNameList");
+let animate = document.querySelectorAll(".animate");
+generateBtn.addEventListener("click", generateColor);
+
+function generateColor() {
+  for (let i = 0; i < colorPlace.length; i++) {
+    const randomColor = Math.floor(Math.random() * 122000).toString(16);
+    colorPlace[i].style.background = "#" + randomColor;
+    colorName[i].textContent = "#" + randomColor;
+    colorPlace[i].classList.remove("animate");
+  }
+}
