ed317e52923417f0dbc6e33a2c76bbf4a72c11ae
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-03-11 17:56:31 +0430

    fix all codes and finall commit

--- app.js
+++ app.js
@@ -6,34 +6,18 @@ let animate = document.querySelectorAll(".animate");
 let colorInp = document.getElementById("colorInp");
 generateBtn.addEventListener("click", () => {
   let color = colorInp.value.slice(1, 7);
+  alert("the response should take time!!");
   colorGenerator(color);
 });
-
-// function generateColor() {
-//   for (let i = 0; i < colorPlace.length; i++) {
-//     const randomColor = Math.floor(Math.random() * 124777).toString(16);
-//     colorPlace[i].style.background = "#" + randomColor;
-//     colorName[i].textContent = "#" + randomColor;
-//     colorPlace[i].classList.remove("animate");
-//   }
-// }
 
 function colorGenerator(color) {
   fetch(`https://www.thecolorapi.com/scheme?hex=${color}&format=json`)
     .then((response) => response.json())
     .then((data) => {
-      data.colors.map((color) => {
-        for (let i = 0; i < colorPlace.length; i++) {
-          console.log(i);
-          colorPlace[i].style.background = color.hex.value;
-          colorName[i].textContent = color.hex.value;
-          colorPlace[i].classList.remove("animate");
-        }
+      data.colors.map((color, index) => {
+        colorPlace[index].style.background = color.hex.value;
+        colorName[index].textContent = color.hex.value;
+        colorPlace[index].classList.remove("animate");
       });
-
-      console.log(data);
-      // for (let scheme of data.colors) {
-      //   console.log(`${scheme.name}: ${scheme.hex.value}`);
-      // }
     });
 }
